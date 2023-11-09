e4658bca8dd4633c5337404676431f737d403047
Austin Allen <otonomee@gmail.com>
2023-11-08 23:09:06 -0700

    into flask app

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
-'''
-This is the main module of the web app that integrates the demucs ml model and the youtube-dl package.
-'''
-import tkinter as tk
-from tkinter import messagebox
+from flask import Flask, request, render_template, redirect, url_for
 from downloader import Downloader
 from demucs_processor import DemucsProcessor
-class App:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.title("YouTube Audio Converter")
-        self.url_entry = tk.Entry(self.root)
-        self.url_entry.pack()
-        self.convert_button = tk.Button(self.root, text="Convert", command=self.convert_audio)
-        self.convert_button.pack()
-        self.downloader = Downloader()
-        self.demucs_processor = DemucsProcessor()
-    def convert_audio(self):
-        youtube_url = self.url_entry.get()
+
+app = Flask(__name__)
+downloader = Downloader()
+demucs_processor = DemucsProcessor()
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        youtube_url = request.form.get('url')
+        print(youtube_url)
         if not youtube_url:
-            messagebox.showerror("Error", "URL is required.")
-            return
-        video_filename = self.downloader.download_video(youtube_url)
+            return render_template('index.html', error="URL is required.")
+        video_filename = downloader.download_video(youtube_url)
         if not video_filename:
-            messagebox.showerror("Error", "Failed to download video.")
-            return
-        audio_filename = self.downloader.get_output_filename(video_filename)
+            return render_template('index.html', error="Failed to download video.")
+        audio_filename = downloader.get_output_filename(video_filename)
         if not audio_filename:
-            messagebox.showerror("Error", "Failed to get output filename.")
-            return
-        self.demucs_processor.process_audio(audio_filename)
-    def run(self):
-        self.root.mainloop()
+            return render_template('index.html', error="Failed to get output filename.")
+        demucs_processor.process_audio(audio_filename)
+        return redirect(url_for('download', filename=audio_filename))
+    return render_template('index.html')
+
+# @app.route('/download/<filename>')
+# def download(filename):
+#     # Implement your file download logic here
+#     pass
+
 if __name__ == "__main__":
-    app = App()
-    app.run()
+    app.run(debug=True)
