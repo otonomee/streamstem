b8c18c0cecc6f8464fc235562ee1e2595587e2ca
Austin Allen <otonomee@gmail.com>
2023-11-27 21:51:27 -0700

    release ready

--- main.py
+++ main.py
@@ -12,30 +12,23 @@ from demucs_processor import DemucsProcessor
 from spotify_to_yt import ConvertSpofity
 import os
 import glob
+import glob
+import shutil
 
 app = Flask(__name__)
 global filename
-
-# downloader = Downloader()
 demucs_processor = DemucsProcessor()
 downloader = Downloader()
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/", methods=["GET", "POST"])
 def home():
     refresh_directories()
     return render_template("index.html")
 
 
-import os
-import shutil
-import glob
-
-
 @app.route("/delete", methods=["POST", "GET"])
 def delete():
-    # clear the contents of /tracks/htdemucs/ and /tracks/htdemucs_6s/
-
     return render_template("index.html")
 
 
@@ -54,11 +47,6 @@ def download_audio():
     return jsonify({"status": "success", "filename": str(filename)})
 
 
-def progress_check(d):
-    if d["status"] == "finished":
-        print("RUN DEMUCS")
-
-
 @app.route("/process_audio", methods=["POST"])
 def process_audio():
     filename = request.json.get("filename")
@@ -70,20 +58,13 @@ def process_audio():
 
 @app.route("/download", methods=["POST", "GET"])
 def download():
+    print("/download it")
     filename = request.args.get("filename")
-    response = send_file(f"{filename}", as_attachment=True)
+    response = send_file(f"{filename}.zip", as_attachment=True)
 
     # Delete the .zip file after sending it
     if os.path.exists(f"{filename}.zip"):
         os.remove(f"{filename}.zip")
-
-    # Delete any .mp3 or .wav files
-    for file in glob.glob("*.mp3"):
-        os.remove(file)
-    for file in glob.glob("*.wav"):
-        os.remove(file)
-    for file in glob.glob("*flac"):
-        os.remove(file)
 
     return response
 
@@ -119,6 +100,13 @@ def refresh_directories():
     for directory in glob.glob("tracks/htdemucs_6s/*"):
         if os.path.isdir(directory):
             shutil.rmtree(directory)
+    # Delete any .mp3 or .wav files
+    for file in glob.glob("*.mp3"):
+        os.remove(file)
+    for file in glob.glob("*.wav"):
+        os.remove(file)
+    for file in glob.glob("*flac"):
+        os.remove(file)
 
 
 if __name__ == "__main__":
