7c535cc2a7b74b45e8f10ddd888e2b18b5a33cbc
Austin Allen <otonomee@gmail.com>
2023-12-18 00:05:27 -0700

    fix downloader

--- downloader.py
+++ downloader.py
@@ -3,7 +3,7 @@ import subprocess
 class Downloader:
     def download_video(self, youtube_url, filetype):
         try:
-            subprocess.run(["yt-dlp", "--extract-audio", "--audio-format", filetype, "-o", "%(title)s", youtube_url, "--force-ipv6"], check=True)
+            subprocess.run(["yt-dlp", "--extract-audio", "--audio-format", filetype, "-o", "%(title)s", "--source-address", "0.0.0.0", youtube_url], check=True)
             output_filename = self.get_output_filename(youtube_url)
             return str(output_filename)
         except subprocess.CalledProcessError:
@@ -11,7 +11,7 @@ class Downloader:
 
     def get_output_filename(self, youtube_url):
         try:
-            output = subprocess.check_output(["yt-dlp", "--get-filename", "-o", "%(title)s", youtube_url])
+            output = subprocess.check_output(["yt-dlp", "--get-filename", "-o", "%(title)s", "--source-address", "0.0.0.0", youtube_url])
             return output.decode("utf-8").strip()
         except subprocess.CalledProcessError:
             return None
